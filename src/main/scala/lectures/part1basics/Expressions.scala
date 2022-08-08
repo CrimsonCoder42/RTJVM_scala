ab8eb8680e65a5b4f598f7267fa350968f75b1a4
CrimsonCoder42 <nostro37@gmail.com>
2022-08-08 07:22:45 -0400

    expressions

---
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -0,0 +1,19 @@
+package lectures.part1basics
+
+object Expressions extends App{
+
+  val x = 1 + 2 // Expression
+
+  println(x)
+
+  val y = 3
+
+  println(y != x)
+
+
+
+  val condition = true
+
+  val aConditionedValue
+
+}
---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,35 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+
+  println(x)
+
+// VALS ARE IMMUTABLE
+
+// COMPILER can infer types
+
+// VALS ARE IMMUTABLE
+
+  val aString: String = "hello"
+
+  println(aString)
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 234455678907L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables are mutable
+
+  var aVariable: Int = 4
+
+  aVariable
+
+
+
+}
