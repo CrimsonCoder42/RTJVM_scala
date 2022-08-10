65b532ed0c4462b3dc486523ba263435bf3cb8ed
CrimsonCoder42 <nostro37@gmail.com>
2022-08-10 09:47:56 -0400

    OOP basics challenges

--- src/main/scala/lectures/part2OOP/OOBasics.scala
+++ src/main/scala/lectures/part2OOP/OOBasics.scala
@@ -4,11 +4,17 @@ object OOBasics extends App {
 
   //val person = new Person("John", 26)
 
-  val writer = new Writer("Devin", "Anderson", 1983 )
+  val author = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
 
-  writer.fullName()
+  println(novel.authorAge)
+  println(novel.isWrittenzBy(author))
 
-  val novel = new Novel("Devin's Tale", 2022, writer)
+  val counter = new Counter
+
+  counter.inc.print
+
+
 
 }
 
@@ -41,6 +47,36 @@ class Writer(fname: String, surname: String, val year: Int) {
 class Novel(name: String, year: Int, author: Writer) {
 
   def authorAge = year - author.year
-  def isWritten
+  def isWrittenzBy(author: Writer) = author == this.author
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+
+}
+
+
+class Counter(val count: Int = 0) {
+
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1)
+  } // immutability
+
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n:Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n-1)
+
+  } // immutability
+
+
+  def dec(n:Int): Counter = {
+    if (n <= 0) this
+    else dec.dec(n-1)
+  }
+
+  def print = println(count)
 
 }
